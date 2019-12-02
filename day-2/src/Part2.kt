853ed14eb19a8375c0ca7fc00b62e1645821f361
woojiahao <woojiahao@gmail.com>
2019-12-02 22:08:25 +0800

    feature: refactor change code

--- day-2/src/Part2.kt
+++ day-2/src/Part2.kt
@@ -13,7 +13,13 @@ fun change(data: List<Int>, noun: Int, verb: Int) =
     replace(2, verb)
   }
 
-fun solve2(data: List<Int>, expected: Int): Pair<Int, Int> {
+fun changePure(data: List<Int>, noun: Int, verb: Int) =
+  data.modify {
+    set(1, noun)
+    set(2, verb)
+  }
+
+fun solve2(data: List<Int>, change: (List<Int>, Int, Int) -> List<Int>, expected: Int): Pair<Int, Int> {
   for (noun in 0..99) {
     for (verb in 0..99) {
       if (solve(change(data, noun, verb), 0) == expected) return Pair(noun, verb)
@@ -23,10 +29,15 @@ fun solve2(data: List<Int>, expected: Int): Pair<Int, Int> {
   throw Exception("Failed to calculate a valid noun and verb")
 }
 
-fun main() {
+fun time(data: List<Int>, change: (List<Int>, Int, Int) -> List<Int>, expected: Int): Long {
   val before = System.currentTimeMillis()
-  val (noun, verb) = solve2(data, 19690720)
+  solve2(data, change, expected)
   val after = System.currentTimeMillis()
-  println(after - before)
-  println(100 * noun + verb)
+  return after - before
+}
+
+fun main() {
+  println(time(data, ::changeImmutable, 19690720))
+  println(time(data, ::change, 19690720))
+  println(time(data, ::changePure, 19690720))
 }
