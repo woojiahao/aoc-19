cf8d9267c0563246b4f26b219830d90fccf7e780
woojiahao <woojiahao@gmail.com>
2019-12-06 13:08:34 +0800

    add: dualWith to shorten returns

--- day-3/src/Utility.kt
+++ day-3/src/Utility.kt
@@ -4,3 +4,5 @@ fun data() =
   with(File("data.txt").readLines()) {
     Pair(readTravel(this[0]), readTravel(this[1]))
   }
+
+fun <U, V, R> dualWith(first: U, second: V, body: (U, V) -> R) = body(first, second)
