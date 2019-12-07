58a8753cb0b3e586313cb4b4426dfde86cd871a3
woojiahao <woojiahao1234@gmail.com>
2019-12-07 10:21:25 +0800

    add: timeAndAnswer function to shorten main()

--- day-4/src/Utility.kt
+++ day-4/src/Utility.kt
@@ -1,2 +1,14 @@
+import kotlin.system.measureTimeMillis
+
 const val lower = 284639
 const val upper = 748759
+
+fun timeAndAnswer(method: (Int, Int) -> Int) {
+  val answer = method(lower, upper)
+  println("Answer: $answer")
+
+  val time = measureTimeMillis {
+    method(lower, upper)
+  }
+  println("Time taken: $time")
+}
